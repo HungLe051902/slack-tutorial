d280746a95fbc2ea5c90169eed61d43ed2420e35
Your Name <your.email@example.com>
2024-10-13 15:41:53 +0700

    add login with github

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,5 +1,6 @@
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
+import { useAuthActions } from "@convex-dev/auth/react"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
@@ -14,8 +15,14 @@ interface SignInCardProps {
 }
 
 export const SignInCard = ({setState}: SignInCardProps) => {
+    const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const handleProviderSignIn = (value: 'github' | 'google') => {
+        signIn(value);
+    }
 
     return (
         <Card className="w-full h-full p-8">
@@ -53,7 +60,7 @@ export const SignInCard = ({setState}: SignInCardProps) => {
                 <div className="flex flex-col gap-y-2.5">
                     <Button
                         disabled={false}
-                        onClick={() => {}}
+                        onClick={() => handleProviderSignIn("google")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
@@ -63,7 +70,7 @@ export const SignInCard = ({setState}: SignInCardProps) => {
                     </Button>
                     <Button
                         disabled={false}
-                        onClick={() => {}}
+                        onClick={() => handleProviderSignIn("github")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
 import { convexAuthNextjsMiddleware, createRouteMatcher, isAuthenticatedNextjs, nextjsMiddlewareRedirect } from "@convex-dev/auth/nextjs/server";
 
-const isPublicPage = createRouteMatcher(["/signin"]);
+const isPublicPage = createRouteMatcher(["/auth"]);
 
 export default convexAuthNextjsMiddleware((request) => {
     if(!isPublicPage(request) && !isAuthenticatedNextjs()) {
-        return nextjsMiddlewareRedirect(request, "/signin");
+        return nextjsMiddlewareRedirect(request, "/auth");
+    }
+
+    if(isPublicPage(request) && isAuthenticatedNextjs()) {
+        return nextjsMiddlewareRedirect(request, "/");
     }
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }
